ef3fa063db03cdec9a5e1818cceea432d380d618
garycarpenter <113570311+garycarpenter@users.noreply.github.com>
2023-03-16 06:38:14 -0700

    yay!

--- caeser_cipher.rb
+++ caeser_cipher.rb
@@ -2,16 +2,32 @@
 
 # function takes a string and a shift factor and returns rotated string
 
-# TODO make z loop to a and Z loop to A
+# DONE make z loop to a and Z loop to A
 # DONE ignore spaces and punctionation mark
+# TODO write tests for code
+
 
 def caesar_cipher(string_to_shift, shift_factor)
     p string_to_shift
     p shift_factor
     shifted_string = ""
+
+    lowercase = ("a".."z").to_a
+    uppercase = ("A".."Z").to_a
+
+    p lowercase
+    p uppercase
+
     string_to_shift.each_char do | char |
-        if ("a"..."z").include?(char) || ("A"..."Z").include?(char)
-            char = char.ord + shift_factor
+        if lowercase.include?(char)
+            char_index = lowercase.find_index(char)
+            shifted_lowercase = lowercase.rotate(shift_factor)
+            char = shifted_lowercase[char_index]
+            shifted_string << char
+        elsif uppercase.include?(char)
+            char_index = uppercase.find_index(char)
+            shifted_uppercase = uppercase.rotate(shift_factor)
+            char = shifted_uppercase[char_index]
             shifted_string << char
         else
             shifted_string << char
