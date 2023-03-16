c6827c93386b6246734ea9ebdc8e5b659902ddcf
garycarpenter <113570311+garycarpenter@users.noreply.github.com>
2023-03-15 22:26:30 -0700

    update caesar_cipher.rb

---
+++ caeser_cipher.rb
@@ -0,0 +1,9 @@
+# caeser_cipher.rb
+
+# function takes a string and a shift factor and returns rotated string
+def caesar_cipher(string_to_shift, shift_factor)
+    p string_to_shift
+    p string_factor
+end
+
+caesar_cipher("What a string!", 5)
